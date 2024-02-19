1890df7322e186bf00a96b554145e7492315a3c9
iain <iainquayle@outlook.com>
2024-02-19 16:31:52 -0700

    home route

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"net/http"
-
 	"github.com/iainquayle/armfinity/components"
+	"github.com/a-h/templ"
+	"fmt"
 )
 
 func main() {
-
+	http.Handle("/", templ.Handler(components.Home()))
+	fmt.Println("Server started on port 8080")
+	http.ListenAndServe(":8080", nil)
 }
